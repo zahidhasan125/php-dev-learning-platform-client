89e2f73b038204717441889c17f0108c5fbd022f
zahidhasan125 <zahidhasan125@gmail.com>
2023-01-24 06:28:39 +0600

    polish done

--- src/pages/Home/CourseList/CourseList.js
+++ src/pages/Home/CourseList/CourseList.js
@@ -10,7 +10,7 @@ const CourseList = ({ courseList }) => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-4'>
                 {
 
-                    courseList.map(course => <Link to={`/courses/details/${course.id}`} className='flex text-center text-white font-bold text-lg p-2 rounded-md bg-gradient-to-r from-emerald-700 via-green-700 to-lime-700 dark:from-stone-900 dark:via-zinc-900 dark:to-slate-900 border-none btn btn-outline h-auto'><BiRightArrow className='text-white' /><button className='flex-1'>{course.name}</button></Link>)
+                    courseList.map(course => <Link to={`/courses/details/${course.id}`} className='flex text-center text-white font-bold text-lg p-2 rounded-md bg-gradient-to-r from-emerald-700 via-green-700 to-lime-700 dark:from-stone-900 dark:via-zinc-900 dark:to-slate-900 border-none btn btn-outline h-auto hover:translate-x-2'><BiRightArrow className='text-white' /><button className='flex-1'>{course.name}</button></Link>)
                 }
             </div>
         </div>
--- src/pages/Home/UnlockPremium/UnlockPremium.js
+++ src/pages/Home/UnlockPremium/UnlockPremium.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const UnlockPremium = () => {
     return (
-        <div className='flex flex-col md:flex-row items-center justify-center text-white my-8 py-8 px-4 border border-gray-500 rounded-md gap-5'>
+        <div className='flex flex-col md:flex-row items-center justify-center text-gray-900 dark:text-white my-8 py-8 px-4 border border-gray-500 rounded-md gap-5'>
             <div className='flex flex-col items-center justify-center w-full'>
                 <h3 className='text-center text-3xl font-bold mb-3'>Get Premium Access</h3>
                 <p className='text-center text-lg'>Get access to all lessons taught by our experts.</p>
             </div>
             <div className='flex flex-col items-center justify-center w-full'>
-                <button className='btn btn-outline text-white mb-3'>Start Course</button>
+                <button className='btn btn-outline mb-3 dark:hover:text-black dark:hover:bg-white dark:text-white'>Start Course</button>
                 <p>Have an account? <Link to="/login" className='underline'>Login</Link> </p>
             </div>
         </div>
